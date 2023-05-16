fad1688c02985f0dffb176e6be20435f3292581d
Raphael da Silva <raphael_correa_silva@hotmail.com>
2023-05-16 15:04:03 -0300

    Criação dos seeds para o db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,32 @@ class DatabaseSeeder extends Seeder
     {
         //\App\Models\User::factory(10)->create(); // Cria 10 usuários fake de exemplo, não necessário
 
+        // Seeder para usuarios
         \App\Models\User::factory()->create([
-            'name' => 'Usuário de Teste',
-            'email' => 'test@example.com',
+            'name' => 'Usuário #1',
+            'email' => 'usuario1@exemplo.com',
             'password' => Hash::make('1234')
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Usuário #2',
+            'email' => 'usuario2@exemplo.com',
+            'password' => Hash::make('1234')
+        ]);
+
+        // Seeder para reservas
+        \App\Models\ReservaEfetuada::create([
+            'id_usuario' => 1, // Usuário 1
+            'numero_da_mesa' => 5,
+            'dia' => date('Y-m-d'),
+            'horario' => '21:00'
+        ]);
+
+        \App\Models\ReservaEfetuada::create([
+            'id_usuario' => 2, // Usuário 2
+            'numero_da_mesa' => 2,
+            'dia' => date('Y-m-d', strtotime('+1 day')), // reserva para o dia seguinte
+            'horario' => '23:50'
         ]);
     }
 }
